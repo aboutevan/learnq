be549148acfdbd3c50c58bd46f65a9d89c1405cb
Evan Salzbrenner <evan@Evans-iMac.local>
2017-07-19 09:40:51 -0700

    start 5

--- 05.AJAX_Request/src/ajaxRequest.js
+++ 05.AJAX_Request/src/ajaxRequest.js
@@ -1,4 +1,12 @@
-function ajaxReq() {
+function ajaxReq(url, options) {
   'use strict';
-  // code goes here
+
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) console.log(request.status);
+  }
+  request.open(options.method, url);
+  request.send();
+
 }
